08e1a80d85ffe9fc8154ced423e336ca2ca23ec2
allef (garug) <allefff@gmail.com>
2026-01-06 12:22:32 -0300

    test cron

--- main.ts
+++ main.ts
@@ -3,3 +3,7 @@ import { handleInteraction } from "@infra/discord/interactions/handler.ts";
 Deno.serve((req) => {
   return handleInteraction(req);
 });
+
+Deno.cron("spawn-tick", "* * * * *", async () => {
+  console.log(new Date())
+});
