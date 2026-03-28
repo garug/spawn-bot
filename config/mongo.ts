7c4c6dcdc69ee01efd025e299c4fb9feb7a444d4
garug <allefff@gmail.com>
2026-03-28 11:14:39 -0300

    mongo connect step

--- config/mongo.ts
+++ config/mongo.ts
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import { env } from "./env.ts";
+import { traced } from "@infra/telemetry.ts";
 
 let connectionPromise: Promise<void> | null = null;
 
@@ -7,7 +8,7 @@ export async function connectDatabase(): Promise<void> {
     if (mongoose.connection.readyState === 1) return;
     if (connectionPromise) return connectionPromise;
 
-    connectionPromise = (async () => {
+    connectionPromise = traced("mongo.connect", async () => {
         try {
             console.log("Connecting to database...");
             await mongoose.connect(env.mongoUri);
@@ -17,7 +18,7 @@ export async function connectDatabase(): Promise<void> {
             console.error("Error connecting to database", error);
             throw error;
         }
-    })();
+    });
 
     return connectionPromise;
 }
